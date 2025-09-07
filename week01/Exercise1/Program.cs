16fedb37a026539f862b9c98102b5189b7cdc489
erncod <ezetobechukwu@gmail.com>
2025-09-07 23:47:54 +0100

    run code successfuly

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,18 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-   
-  {
+    static void Main()
+    {
         // Prompt the user for their first name
         Console.Write("What is your first name? ");
-        string firstName = Console.ReadLine(Ernest);
+        string firstName = Console.ReadLine();
 
         // Prompt the user for their last name
         Console.Write("What is your last name? ");
-        string lastName = Console.ReadLine(Tobechukwu);
+        string lastName = Console.ReadLine();
 
         // Display the formatted output
-        Console.WriteLine($"Your name is {Tobechukwu}, {Ernest} {Tobechukwu}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
-}   
+}
